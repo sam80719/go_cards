cb97aab01660140e771e4f3e26ded9898a049db7
chenmingsan <sam80719@gmail.com>
2022-12-09 15:17:33 +0800

    完成檔案的測試

--- deck_test.go
+++ deck_test.go
@@ -1,11 +1,36 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestNewDeck(t *testing.T) {
 	d := newDeck()
 
 	if len(d) != 16 {
-		t.Errorf("Length is not 16, got %v", len(d)) //
+		t.Errorf("Length is not 16, got %v", len(d))
 	}
+
+	if d[0] != "Ace of Spades" {
+		t.Errorf("First card is not Ace of Spades, got %v", d[0])
+	}
+
+	if d[len(d)-1] != "Four of Clubs" {
+		t.Errorf("Last card is not Four of Clubs, got %v", d[len(d)-1])
+	}
+}
+
+func TestSaveToDeckAndNewDeckFromFile(t *testing.T) {
+	os.Remove("_deckTesting!")
+
+	deck := newDeck()
+	deck.saveToFile("_deckTesting")
+
+	loadedDeck := newDeckFromFile("_deckTesting")
+
+	if len(loadedDeck) != 16 {
+		t.Errorf("expected 16 cards in deck, got %v", len(loadedDeck))
+	}
+	os.Remove("_deckTesting")
 }
